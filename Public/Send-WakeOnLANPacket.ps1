b2d22bcbc6917a669c034c0be66e4a5c0f3f763c
WPTechnician <crobitaille@waypointcentre.ca>
2018-11-22 11:34:28 -0500

    Update Send-WakeOnLANPacket.ps1
    Just thought a quick check on the MAC address format would be useful.

--- Public/Send-WakeOnLANPacket.ps1
+++ Public/Send-WakeOnLANPacket.ps1
@@ -15,15 +15,22 @@ Function Send-WakeOnLANPacket {
     Begin {}
 
     Process {
-
-        $MACArray = $_ -split "[:-]" | ForEach-Object { [Byte]"0x$_"}
-        [Byte[]]$WoLPacket = (, 0xFF * 6) + ($MACArray * 16)
-        $Client = New-Object System.Net.Sockets.UDPClient
-        $Client.Connect(([System.Net.IPAddress]::Broadcast), 7)
-        [void]$Client.Send($WoLPacket, $WoLPacket.Length)
-        $Client.Close()
-        $s = [Scriptblock]::Create("Foo")
-        $s.Invoke()
+        $regex = 
+        if($_ -match "/^(?:[[:xdigit:]]{2}([-:]))(?:[[:xdigit:]]{2}\1){4}[[:xdigit:]]{2}$/")
+        {
+            $MACArray = $_ -split "[:-]" | ForEach-Object { [Byte]"0x$_"}
+            [Byte[]]$WoLPacket = (, 0xFF * 6) + ($MACArray * 16)
+            $Client = New-Object System.Net.Sockets.UDPClient
+            $Client.Connect(([System.Net.IPAddress]::Broadcast), 7)
+            [void]$Client.Send($WoLPacket, $WoLPacket.Length)
+            $Client.Close()
+            $s = [Scriptblock]::Create("Foo")
+            $s.Invoke()
+        }
+        else
+        {
+            write-host "An invalid MAC Address was entered!"
+        }
     }
 
 }
